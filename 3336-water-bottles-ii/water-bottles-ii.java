c994fb7782b783a18aa0977afac35ce4610d418f
Anshukumari024 <anshurajyadav.6639@gmail.com>
2025-10-02 11:55:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (73.55%) - LeetSync

--- 3336-water-bottles-ii/water-bottles-ii.java
+++ 3336-water-bottles-ii/water-bottles-ii.java
@@ -1,10 +1,8 @@
 class Solution {
     public int maxBottlesDrunk(int numBottles, int numExchange) {
-       int f=numBottles;
-       int e=0;
-       int s=f;
-       e=e+f;
-       f=0;
+       int s=numBottles;
+       int e=numBottles;
+       int f=0;
        while(e>0){
         if(e<numExchange) break;
         e=e-numExchange;
