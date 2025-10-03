96e093c6ba50c3c40fc570bfcf9a1ad1d9c8c2d7
Anshukumari024 <anshurajyadav.6639@gmail.com>
2025-10-03 11:42:09 +0530

    Time: 1 ms (99.62%) | Memory: 44.3 MB (89.75%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,15 +1,32 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-    Set s = new HashSet();
-    for (int i=0;i<9;i++) {
-        for (int j=0;j<9;j++) {
-            if (board[i][j] != '.') {
-                String b = "(" + board[i][j] + ")";
-                if (!s.add(b+i) || !s.add(j+b) ||!s.add(i/3+b+j/3))
-                    return false;
+        return sudoku(board,9);
+        
+    }
+    public boolean sudoku(char[][] board,int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]!='.'){
+                    if(!isValid(board,board[i][j],i,j,n)) return false;
+                }
             }
         }
+        return true;
     }
-    return true;
-}
+    public boolean isValid(char[][] board,char ch,int r,int c,int n){
+        for(int i=r+1;i<n;i++){
+            if(board[i][c]==ch) return false;
+        }
+        for(int i=c+1;i<n;i++){
+            if(board[r][i]==ch) return false;
+        }
+        int rr=r-(r%3);
+        int cc=c-(c%3);
+        for(int i=rr;i<rr+3 && i<9;i++){
+            for(int j=cc;j<cc+3 && j<9;j++){
+                if(board[i][j]==ch && i!=r && j!=c) return false;
+            }
+        }
+        return true;
+    }
 }
