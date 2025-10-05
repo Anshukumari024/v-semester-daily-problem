1141845aa9c782b4eb0576dab5c14c96ec8cbb70
Anshukumari024 <anshurajyadav.6639@gmail.com>
2025-10-05 18:12:00 +0530

    Time: 1 ms (49.29%) | Memory: 41.5 MB (21.16%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,17 +1,20 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
+        int[][] dp=new int[23][23];
+        for(int[] arr:dp) Arrays.fill(arr,-1);
         int n=nums.length;
         int total=Arrays.stream(nums).sum();
-        int p1=solve(0,n-1,nums);
+        int p1=solve(0,n-1,nums,dp);
         int p2=total-p1;
         if(p1>=p2) return true;
         return false;
     }
-    public int solve(int i,int j, int[] nums){
+    public int solve(int i,int j, int[] nums,int[][] dp){
         if(i>j) return 0;
         if(i==j) return nums[i];
-        int take_i=nums[i]+Math.min(solve(i+2,j,nums),solve(i+1,j-1,nums));
-        int take_j=nums[j]+Math.min(solve(i+1,j-1,nums),solve(i,j-2,nums));
-        return Math.max(take_i,take_j);
+        if(dp[i][j]!=-1) return dp[i][j];
+        int take_i=nums[i]+Math.min(solve(i+2,j,nums,dp),solve(i+1,j-1,nums,dp));
+        int take_j=nums[j]+Math.min(solve(i+1,j-1,nums,dp),solve(i,j-2,nums,dp));
+        return dp[i][j]=Math.max(take_i,take_j);
     }
 }
